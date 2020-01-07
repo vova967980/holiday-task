f529c77454c9e4e56a7067eacb5c1b7fb09fb70c
VladimirS <vova967980@gmail.com>
2020-01-07 18:23:09 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -55,21 +55,32 @@ elementOfLi[4].onclick=closeMenu;
 
 let images = document.getElementsByClassName("workContainer");
 let imagesOpen = document.getElementsByClassName("imageOpen");
+let currentImage = document.getElementById("currentImageWorks");
 let backgroundDark = document.getElementsByClassName("backgroundDark");
 let closeButton = document.getElementsByClassName("closeIcon");
+let imagesSrc = [
+  "./assets/images/work-1.jpg",
+  "./assets/images/work-2.jpg",
+  "./assets/images/work-3.jpg",
+  "./assets/images/work-4.jpg",
+  "./assets/images/work-5.jpg",
+  "./assets/images/work-6.jpg",
+  "./assets/images/work-7.jpg",
+  "./assets/images/work-8.jpg"
+];
 
-
-for (let i=0;i<images.length;i++){
-    images[i].onclick=function () {
-        imagesOpen[i].classList.add("openImage");
+for (let i=0;i<images.length;i++) {
+    images[i].onclick = function () {
+        imagesOpen[0].classList.add("openImage");
+        currentImage.src = imagesSrc[i];
     };
-    backgroundDark[i].onclick=function () {
-        imagesOpen[i].classList.remove("openImage");
-    };
-    closeButton[i].onclick=function () {
-        imagesOpen[i].classList.remove("openImage")
-    }
 }
+backgroundDark[0].onclick = function () {
+    imagesOpen[0].classList.remove("openImage");
+};
+closeButton[0].onclick = function () {
+    imagesOpen[0].classList.remove("openImage");
+};
 
 //=====================================================================
 
@@ -89,7 +100,6 @@ const loadData = async function(){
 };
 
 loadData();
-
 
 const iconsContacts =['fa-facebook','fa-twitter','fa-linkedin','fa-google-plus','fa-dribbble'];
 
