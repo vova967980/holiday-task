427a91b28fa54bb57d25de3a1c38b4d939ddf900
VladimirS <vova967980@gmail.com>
2020-01-16 18:33:10 +0200

    Slider, set max count of slides

--- src/assets/js/Slider/index.js
+++ src/assets/js/Slider/index.js
@@ -8,10 +8,12 @@ export default function (slides) {
 
     const inputsContainer = document.querySelector('.inputsContainer');
     const containerSlides = document.getElementById('containerSlides');
-    
+    let iterator = 0;
     for( let slideData of slides ){
         containerSlides.appendChild(createSlide(slideData));
         inputsContainer.appendChild(createInput());
+        iterator++;
+        if(iterator >= 6) break;
     }
     sliderLogic();
 }
